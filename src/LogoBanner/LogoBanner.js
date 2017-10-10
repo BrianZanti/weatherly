705b49a6a2fe4c08a975f3ccf3beffc07f40518b
Brian Zanti <brianzanti@gmail.com>
2017-10-10 15:38:18 -0600

    skeleton for banner

---
+++ src/LogoBanner/LogoBanner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from '../resources/weatherly-logo.png';
+import "./LogoBanner.css";
+
+class LogoBanner extends React.Component {
+	
+	render() {
+		return(
+			<h1>This is the LogoBanner</h1>
+		);
+	}
+}
+
+export default LogoBanner;
