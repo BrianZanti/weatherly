18392a08ecfe0c541300be0fd2f1a6411e311bbf
Brian Zanti <brianzanti@gmail.com>
2017-10-11 16:36:48 -0600

    Arranged components on page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import LogoBanner from './LogoBanner/LogoBanner';
+import SearchBar from './SearchBar/SearchBar';
+import Welcome from './Welcome/Welcome';
+import CurrentWeather from './CurrentWeather/CurrentWeather';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App">      
+        <div className="LogoBanner">
+          <LogoBanner />
+        </div>        
+        <div className="SearchBarDisplay">
+          <SearchBar />
+        </div>
+        <div className="WeatherInfoDisplay">  
+          <CurrentWeather />          
+          <CurrentWeather />
+        </div>        
       </div>
     );
   }
