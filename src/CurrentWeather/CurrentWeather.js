e92267a7a6b79a25b13d2b02aa012a51e9d6297c
Brian Zanti <brianzanti@gmail.com>
2017-10-11 13:20:05 -0600

    skeleton for CurrentWeather

---
+++ src/CurrentWeather/CurrentWeather.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./CurrentWeather.css";
+
+class CurrentWeather extends React.Component {	
+
+	render() {
+		return(
+			
+		);
+	}
+}
+
+export default CurrentWeather;
