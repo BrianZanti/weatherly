60cdcd727f717ff3f99303f0c67875493416cb34
Brian Zanti <brianzanti@gmail.com>
2017-10-12 20:06:16 -0600

    CurrentWeather now accept data through props

--- src/CurrentWeather/CurrentWeather.js
+++ src/CurrentWeather/CurrentWeather.js
@@ -3,19 +3,25 @@ import "./CurrentWeather.css";
 
 class CurrentWeather extends React.Component {	
 
+	constructor(props){
+		super(props);
+		this.state = {
+			weatherData: {}
+		}
+	}
+
 	render() {
 		return(
 			<div className="CurrentWeather">
-				<h1 className="CityName">SomeCity</h1>
-				<p className="StateAndZip">Some State, 12345</p>
-				<h2 className="Date">Sunday, October 1st</h2>
-				<h3 className="TemperatureHeader">Current Temperature: 70*</h3>
-				<div className="CurrentTemperature">
-						70/32
-						<img src="" alt="Weather Icon" className="CurrentWeatherIcon"/>
+				<h1 className="CityName">{this.props.forecastData.city}</h1>
+				<p className="StateAndZip">{this.props.forecastData.state}, {this.props.forecastData.zip}</p>
+				<h2 className="Date">{this.props.forecastData.date}</h2>
+				<h3 className="TemperatureHeader">Current Temperature: {this.props.observationData.currentTemp}</h3>
+				<div className="Forecast">
+						<h3 className="HighLow">{this.props.forecastData.high}/{this.props.forecastData.low}<img src={this.props.forecastData.icon_url} alt="Weather Icon" className="CurrentWeatherIcon"/></h3>
+						
 				</div>
-				<p className="Summary">Partly generic with a chance of placeholders in the evening</p>
-
+				<p className="Summary">{this.props.forecastData.summary}</p>
 			</div>
 		);
 	}
