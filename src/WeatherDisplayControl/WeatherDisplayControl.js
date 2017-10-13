6a76f6a09733db42fef400dc25a0cd1893d2e181
Brian Zanti <brianzanti@gmail.com>
2017-10-13 12:04:19 -0600

    condensed two api calls into one

--- src/WeatherDisplayControl/WeatherDisplayControl.js
+++ src/WeatherDisplayControl/WeatherDisplayControl.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
+import './WeatherDisplayControl.css';
 import Welcome from '../Welcome/Welcome';
 import CurrentWeather from '../CurrentWeather/CurrentWeather';
 import apiKey from '../wunderground-api-key';
@@ -15,12 +16,10 @@ class WeatherDisplayControl extends Component {
     this.state = {
       location: savedLocation ? savedLocation : '',      
       message: savedLocation ? '' : welcomeMessage,
-      forecastData: {},
-      observationData: {},      
+      weatherData: {},      
     }    
     this.handleLocationUpdate= this.handleLocationUpdate.bind(this);
-    this.apiForecastQuerySuccess = this.apiForecastQuerySuccess.bind(this);
-    this.apiObservationQuerySuccess = this.apiObservationQuerySuccess.bind(this);
+    this.apiQuerySuccess = this.apiQuerySuccess.bind(this);    
     this.apiQueryError = this.apiQueryError.bind(this);    
     if(savedLocation){
       this.handleLocationUpdate(savedLocation);
@@ -34,7 +33,7 @@ class WeatherDisplayControl extends Component {
     })    
 
       let params = submittedText.split(',');                    
-      let url = "http://api.wunderground.com/api/" + apiKey + "/geolookup/forecast/q";
+      let url = "http://api.wunderground.com/api/" + apiKey + "/geolookup/forecast/conditions/q";
       for(let i = params.length - 1; i >= 0; i--){
         url += '/' + params[i].trim();  
       }      
@@ -43,71 +42,48 @@ class WeatherDisplayControl extends Component {
       $.ajax({
         url : url,
         dataType :    "jsonp",
-        success : this.apiForecastQuerySuccess,
+        success : this.apiQuerySuccess,
         error: this.apiQueryError
-      });
-
-      url = url.replace('forecast','conditions');
-
-      $.ajax({
-        url : url,
-        dataType : "jsonp",
-        success : this.apiObservationQuerySuccess,
-        error: this.apiQueryError
-      });    
+      });      
   }
 
-  apiForecastQuerySuccess(parsed_json){        
-    console.log(parsed_json); 
-    
-    if(parsed_json.forecast){            
-      let tempForecastData = {};
+  apiQuerySuccess(parsed_json){     
+    console.log(parsed_json);
 
+    if(parsed_json.forecast){                  
+      let current_observation = parsed_json.current_observation;            
       let location = parsed_json.location;      
-      let forecast = parsed_json.forecast;
-
-      tempForecastData.city = location.city;
-      tempForecastData.state = location.state;
-      tempForecastData.zip = location.zip;      
-      tempForecastData.high = forecast.simpleforecast.forecastday[0].high.fahrenheit;
-      tempForecastData.low = forecast.simpleforecast.forecastday[0].low.fahrenheit;
-      tempForecastData.summary = forecast.txt_forecast.forecastday[0].fcttext;
-      tempForecastData.icon_url = forecast.txt_forecast.forecastday[0].icon_url;
-
-      let date = new Date();
-      let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
-      let postfix = 'th';
-      if(date.getDate() === 1 || date.getDate() === 21 || date.getDate() === 31){
-        postfix = 'st';
-      }
-      if(date.getDate() === 2 || date.getDate() === 22){
-        postfix = 'nd';
-      }
-      if(date.getDate() === 3 || date.getDate() ===23){
-        postfix = 'rd';
-      }
-      tempForecastData.date = days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + postfix;
+      let simpleforecast = parsed_json.forecast.simpleforecast.forecastday[0];
+      let txt_forecast = parsed_json.forecast.txt_forecast.forecastday[0];                  
       
-
-
-      this.setState({
-        forecastData: tempForecastData,
+      this.setState({        
+        weatherData: {
+          currentTemp : current_observation.temp_f,
+          weather : current_observation.weather,   
+          city : location.city,
+          state : location.state,
+          zip : location.zip,  
+          high : simpleforecast.high.fahrenheit,
+          low : simpleforecast.low.fahrenheit,
+          summary : txt_forecast.fcttext,
+          icon_url : txt_forecast.icon_url,
+          date : this.getFormattedDate()
+        },
         message: ''
-      })      
+      });
     }
     else if(parsed_json.response.error){
       //No suggestions or match
       this.setState({
         message: cityNotFoundMessage,
-        forecastData: {}
+        weatherData: {}
       })
     }    
     else if(parsed_json.response.results){ 
       //Suggestions found but no match
       this.setState({
         message: cityNotFoundMessage,
-        forecastData: {}
+        weatherData: {}
       })
     }
     else{
@@ -115,51 +91,35 @@ class WeatherDisplayControl extends Component {
     }      
   }
 
-  apiObservationQuerySuccess(parsed_json){        
-      console.log(parsed_json);
-
-    if(parsed_json.current_observation){                  
-      let tempObservationData = {};
-
-      let current_observation = parsed_json.current_observation;      
-
-      tempObservationData.currentTemp = current_observation.temp_f;
-      tempObservationData.weather = current_observation.weather;      
-
-      this.setState({
-        observationData: tempObservationData,
-        message: ''
-      })      
-    }
-    else if(parsed_json.response.error){
-      this.setState({
-        message: cityNotFoundMessage,
-        observationData: {}
-      })
-    }    
-    else if(parsed_json.response.results){ 
-      this.setState({
-        message: cityNotFoundMessage,
-        observationData: {}
-      })
-    }
-    else{
-      console.log("unknown error");
-    }
-  }
-
   apiQueryError(parrsed_json){
     console.log("api query error");
+  }
+
+  getFormattedDate(){
+    let date = new Date();
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    let months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    let postfix = 'th';
+    if(date.getDate() === 1 || date.getDate() === 21 || date.getDate() === 31){
+      postfix = 'st';
+    }
+    if(date.getDate() === 2 || date.getDate() === 22){
+      postfix = 'nd';
+    }
+    if(date.getDate() === 3 || date.getDate() ===23){
+      postfix = 'rd';
+    }
+    return days[date.getDay()] + ", " + months[date.getMonth()] + " " + date.getDate() + postfix;
   }
 
   render() {        
     let content = "";
 
     if(this.state.message){
-      content = <h2 className="Message">{this.state.message}</h2>
+      content = <h2 className="Message">{this.state.message}</h2>;
     }
-    else if(this.state.observationData.weather){
-      content = <CurrentWeather observationData={this.state.observationData} forecastData={this.state.forecastData}/>
+    else if(this.state.weatherData.weather){
+      content = <CurrentWeather weatherData={this.state.weatherData} />;
     }    
     
     return (
