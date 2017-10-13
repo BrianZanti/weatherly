22ea9e6da4fdd36e3c8182a5c751b0f1f365b61d
Brian Zanti <brianzanti@gmail.com>
2017-10-13 12:03:34 -0600

    condensed props into one weatherData prop

--- src/CurrentWeather/CurrentWeather.js
+++ src/CurrentWeather/CurrentWeather.js
@@ -2,26 +2,20 @@ import React, { Component } from 'react';
 import "./CurrentWeather.css";
 
 class CurrentWeather extends React.Component {	
-
-	constructor(props){
-		super(props);
-		this.state = {
-			weatherData: {}
-		}
-	}
-
+	
 	render() {
 		return(
 			<div className="CurrentWeather">
-				<h1 className="CityName">{this.props.forecastData.city}</h1>
-				<p className="StateAndZip">{this.props.forecastData.state}, {this.props.forecastData.zip}</p>
-				<h2 className="Date">{this.props.forecastData.date}</h2>
-				<h3 className="TemperatureHeader">Current Temperature: {this.props.observationData.currentTemp}</h3>
+				<h1 className="CityName">{this.props.weatherData.city}</h1>
+				<p className="StateAndZip">{this.props.weatherData.state}, {this.props.weatherData.zip}</p>
+				<h2 className="Date">{this.props.weatherData.date}</h2>
+				<h3 className="TemperatureHeader">Current Temperature: {this.props.weatherData.currentTemp}&deg;</h3>
 				<div className="Forecast">
-						<h3 className="HighLow">{this.props.forecastData.high}/{this.props.forecastData.low}<img src={this.props.forecastData.icon_url} alt="Weather Icon" className="CurrentWeatherIcon"/></h3>
+						<p className="HighLow">{this.props.weatherData.high}&deg;/{this.props.weatherData.low}&deg;</p>
+						<img src={this.props.weatherData.icon_url} alt="Weather Icon" className="CurrentWeatherIcon"/>
 						
 				</div>
-				<p className="Summary">{this.props.forecastData.summary}</p>
+				<p className="Summary">{this.props.weatherData.summary}</p>
 			</div>
 		);
 	}
